f2c9714860464edb2a6a399e288a5046673e6358
maxflorentin <macsee13@gmail.com>
2025-07-24 13:45:07 -0300

    some fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Kaizen Website',
+  description: 'Sitio web de Kaizen',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
