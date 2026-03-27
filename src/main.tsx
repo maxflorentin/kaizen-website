3fbe9cb1fa0f6d4f02c9973bfe67bcf847c807a4
Max Florentin <macsee13@gmail.com>
2026-03-27 11:49:42 -0300

    trigger

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'  // Asegúrate de que esta línea esté presente
+import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
